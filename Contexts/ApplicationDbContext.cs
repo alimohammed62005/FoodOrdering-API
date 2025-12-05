4b8dbd6076b5a7ccd124c984243d0245a71b239f
alimohammed62005 <alimohammed62005@gmail.com>
2025-12-05 14:21:04 +0200

    Add files via upload

---
+++ Contexts/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FoodOrdering.Domain.Entities;
+using FoodOrdering.Application.Interfaces;
+
+namespace FoodOrdering.Persistence.Contexts
+{
+    public partial class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<City> City { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Restaurant> Restaurant { get; set; }
+    }
+}
